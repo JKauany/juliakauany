b0a2b81efc57089b58ac575d5f3bb76f47546cdb
JKauany <juliakferreira1@gmail.com>
2023-08-28 08:26:21 -0300

    branch com o código da aula do dia Mon, Aug 28, 2023  8:26:21 AM

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import TelaLogin from './scr/TelaLogin';
+
+export default function App() {
+  return (
+
+    <View style={styles.container}>
+      <TelaLogin></TelaLogin>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
